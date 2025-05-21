5d9deb0ab62585db64afa99d3e1af1c6204cddcb
karcher <karchernic@gmail.com>
2025-05-21 16:18:26 +0200

    removing run_prokka.sh/run_cayman.sh

--- scripts/revisions_aditions/rnaseq/scripts/00_run_prokka.sh
+++
@@ -1,9 +0,0 @@
-#conda activate prokka_env
-export PERL5LIB="/g/typas/Personal_Folders/Nic/miniforge3/envs/prokka_env/lib/perl5/site_perl/"
-mkdir -p ../results/prokka
-#for f in $(ls ../results/assemblies_softlinks/ | sed "s/\.fasta//")
-#for f in $(ls ../results/assemblies_softlinks_selected/ | sed "s/\.fasta//")
-for f in $(ls ../data/genomes/ | grep fna$ | sed "s/\.fna//")
-do
-        echo -e "prokka --cpus 4 --force --outdir ../results/prokka/${f} --prefix ${f} ../data/genomes/${f}.fna"
-done > iv.sh
--- scripts/revisions_aditions/rnaseq/scripts/iv.sh
+++
@@ -1,5 +0,0 @@
-prokka --cpus 4 --force --outdir ../results/prokka/B_uniformis_atcc8492nt5002 --prefix B_uniformis_atcc8492nt5002 ../data/genomes/B_uniformis_atcc8492nt5002.fna
-prokka --cpus 4 --force --outdir ../results/prokka/C_secundus_DSM28864 --prefix C_secundus_DSM28864 ../data/genomes/C_secundus_DSM28864.fna
-prokka --cpus 4 --force --outdir ../results/prokka/E_tayi_DSM26961 --prefix E_tayi_DSM26961 ../data/genomes/E_tayi_DSM26961.fna
-prokka --cpus 4 --force --outdir ../results/prokka/H_hathewayi_DSM13479 --prefix H_hathewayi_DSM13479 ../data/genomes/H_hathewayi_DSM13479.fna
-prokka --cpus 4 --force --outdir ../results/prokka/P_vulgatus_atcc8482nt5001 --prefix P_vulgatus_atcc8482nt5001 ../data/genomes/P_vulgatus_atcc8482nt5001.fna
